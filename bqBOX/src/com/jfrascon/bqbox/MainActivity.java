4aebf664d31bc39106447294a6432cb0b79fa246
jfrascon <jfrascon@gmail.com>
2014-03-15 13:29:42 +0100

    Autenticación con Dropbox.

--- bqBOX/src/com/jfrascon/bqbox/MainActivity.java
+++ bqBOX/src/com/jfrascon/bqbox/MainActivity.java
@@ -1,9 +1,17 @@
 package com.jfrascon.bqbox;
 
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.session.AppKeyPair;
+import com.dropbox.client2.session.Session.AccessType;
+
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,22 +24,60 @@ import android.os.Build;
 
 public class MainActivity extends Activity {
 
+	final static private String APP_KEY = "gxbiyscpmumo7uh";
+	final static private String APP_SECRET = "6qcbw9vp27flg1p";
+
+	final static private String ACCOUNT_PREFS_NAME = "prefs";
+	final static private String ACCESS_TOKEN_NAME = "ACCESS_TOKEN";
+
+	DropboxAPI<AndroidAuthSession> mDBApi;
+
+	Button acceso_dropbox;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		Button acceso_dropbox = (Button) findViewById(R.id.acceso_dropbox);
+
+		acceso_dropbox = (Button) findViewById(R.id.acceso_dropbox);
 		acceso_dropbox.setOnClickListener(new OnClickListener() {
-			
-			
+
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(getApplicationContext(), "Ir a dropbox", Toast.LENGTH_LONG).show();
+				mDBApi.getSession()
+						.startOAuth2Authentication(MainActivity.this);
 			}
 		});
 
+		AppKeyPair appKeys = new AppKeyPair(APP_KEY, APP_SECRET);
+		AndroidAuthSession session = new AndroidAuthSession(appKeys);
+
+		mDBApi = new DropboxAPI<AndroidAuthSession>(session);
+
 	}
-	
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		if (mDBApi.getSession().authenticationSuccessful()) {
+			try {
+				// Required to complete auth, sets the access token on the
+				// session
+				mDBApi.getSession().finishAuthentication();
+
+				String accessToken = mDBApi.getSession().getOAuth2AccessToken();
+
+				if (accessToken != null) {
+					SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+					Editor edit = prefs.edit();
+					edit.putString(ACCESS_TOKEN_NAME, accessToken).commit();
+				}
+
+			} catch (IllegalStateException e) {
+				Log.e(this.getClass().getName(), "Error authenticating", e);
+			}
+		}
+	}
 
 }
